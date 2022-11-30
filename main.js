1f49fef98bc66a6ba42d9a76e9cad0f6c7bd4e07
Laureano <lau.zalazar7@gmail.com>
2022-11-30 01:15:09 -0300

    commit

---
+++ main.js
@@ -0,0 +1,31 @@
+$(function(){
+
+    $("#btn-learn-1").click(function(){
+
+        $(".modal-title").html("Team & Staff Augmentation");
+    });
+    $("#btn-learn-2").click(function(){
+
+        $(".modal-title").html("Product Development& UX");
+    });
+    $("#btn-learn-3").click(function(){
+
+        $(".modal-title").html("Government Solutions");
+    });
+    $("#btn-learn-4").click(function(){
+
+        $(".modal-title").html("Integrity");
+    });
+    $("#btn-learn-5").click(function(){
+
+        $(".modal-title").html("Ownership");
+    });
+    $("#btn-learn-6").click(function(){
+
+        $(".modal-title").html("Flexibility");
+    });
+    $("#btn-learn-7").click(function(){
+
+        $(".modal-title").html("Innovation");
+    });
+});
